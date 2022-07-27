81ae8b7d3f4804f8ad160541997fc0ea10ab8e54
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-07-27 23:04:26 +0300

    changed from class to classname

--- react-application/src/components/Header.js
+++ react-application/src/components/Header.js
@@ -5,31 +5,31 @@ import "./../css/style.css";
 function Header() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item ">
-              <Link class="nav-link" to="/home">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item ">
+              <Link className="nav-link" to="/home">
                 Home
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/about">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
                 About
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/dashboard">
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">
                 Dashboard
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/register">
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
                 SignUp
               </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/login">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
                 Login
               </Link>
             </li>
